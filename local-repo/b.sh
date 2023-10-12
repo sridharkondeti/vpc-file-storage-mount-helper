ddb901c71dd53e565b382a7544e14e83d9029c06
Sridhar Kondeti1 <Sridhar.Kondeti1@ibm.com>
2023-10-12 17:39:23 -0500

    Test repo

--- local-repo/b.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p {RPMS,metadata}
-cp $1 RPMS/
-yum install createrepo -y
-createrepo `pwd`
-echo -e "[local-repo]\nname=Local YUM Repository\nbaseurl=file://`pwd`\nenabled=1\ngpgcheck=0" > /etc/yum.repos.d/local-repo.repo
-yum makecache
